8b13ed3cf8d9d42d4f6e53567b868266858ad03d
Konstantinos Parasyris <parasyris1@llnl.gov>
2023-07-05 12:18:31 -0700

    Lulesh

---
+++ record-replay/RR/benchmarks/lulesh-omp/Lulesh.py
@@ -0,0 +1,42 @@
+import itertools
+import json
+import math
+import os
+import sys
+from pathlib import Path
+import numpy as np
+import argparse
+import re
+
+import RRUtil.Benchmark as Bench
+import RRUtil.Device as Device
+import RRUtil.Utilities as Utilities
+
+class LULESH(Bench.MakeBenchmark):
+    def __init__(self, prof, **kwargs):
+        kwargs.pop("executable", None)
+        kwargs.pop('resultsdir', None)
+        kwargs.pop('root', None)
+        super().__init__(prof,  "lulesh", os.path.split(os.path.realpath(__file__))[0], **kwargs)
+
+    def build(self, fn=None, useCase=None):
+        if fn is None:
+            return super().build({ 'LEFLAGS' : Device.GetCompileOptions(self.Device) })
+
+        leOptions = Device.GetCompileOptions(self.Device) + f' -mllvm --bo-omp-autotune={fn}'
+        return super().build({'LEFLAGS' : leOptions}, useCase)
+
+    def clean(self):
+        return super().clean()
+
+    def command(self):
+        return  f"{self.executableDir}/lulesh -s 128 -r 11 -b 1 -c 1"
+
+    def env(self):
+        return ''
+
+    def getApplicationTime(self, stdout):
+        exec_time_pattern = '__ExecutionTime__:(.*)'
+        tmp =  re.findall(exec_time_pattern, stdout)
+        return tmp[0]
+
